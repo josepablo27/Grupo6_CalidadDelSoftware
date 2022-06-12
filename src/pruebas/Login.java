9c4700688a186e25ea41eee14d6a36fa649b6b26
AndresDiazGarro <iandresfy@gmail.com>
2022-06-11 18:51:10 -0600

    Subida del framework de automatización

---
+++ src/pruebas/Login.java
@@ -0,0 +1,21 @@
+package pruebas;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class Login {
+	
+	public static void main(String[] args) {
+		 
+		
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.automationexercise.com/brand_products/Biba");
+		
+		
+	}
+}
