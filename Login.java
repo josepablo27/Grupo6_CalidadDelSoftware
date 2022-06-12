9ed4a837b0d6411e0ba96c9f828b0f6ee5353386
AndresDiazGarro <iandresfy@gmail.com>
2022-06-11 18:54:37 -0600

    Prueba de cambio

--- Login.java
+++ Login.java
@@ -10,7 +10,7 @@ public class Login {
 	
 	public static void main(String[] args) {
 		 
-		
+		//Modificación
 		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
